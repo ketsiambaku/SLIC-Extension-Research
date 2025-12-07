caa4dafdc2e5f7bcc10307be71d15c299db0cfc2
Chandler Calkins <chandlerjaycalkins@gmail.com>
2025-12-06 17:49:13 -0800

    Added code to super-duper-pixel demo to show clear difference before and after duperizing

--- SuperDuperPixels/src/demo.cpp
+++ SuperDuperPixels/src/demo.cpp
@@ -17,6 +17,37 @@
 #include "sdp_slic.hpp"
 using namespace cv;
 
+// Displays an image with superpixel borders overlayed onto it
+// Inputs:
+// slic: slic object that generated superpixels for input_image
+// input_image: the image that superpixels were generated for using slic
+// window_name: the name of the window to display the superpixels to using cv::imshow()
+Mat show_superpixels(const Ptr<SuperpixelSLIC>& slic, const Mat& input_image, const String& window_name)
+{
+	// Gets overlay image of superpixels
+	Mat superpixels;
+	slic->getLabelContourMask(superpixels);
+
+	// Creates the output image of superpixels
+	Mat output = input_image.clone();
+	// Set each pixel in output to white if it's a superpixel border
+	for (int row = 0; row < output.rows; row += 1)
+	for (int col = 0; col < output.cols; col += 1)
+	{
+		if (superpixels.at<uchar>(row, col) != 0)
+		{
+			output.at<Vec3b>(row, col)[0] = superpixels.at<uchar>(row, col);
+			output.at<Vec3b>(row, col)[1] = superpixels.at<uchar>(row, col);
+			output.at<Vec3b>(row, col)[2] = superpixels.at<uchar>(row, col);
+		}
+	}
+
+	// Displays output to a window
+	imshow(window_name, output);
+	waitKey(0);
+	return output;
+}
+
 // main - Generates superpixels for an images using SD-SLIC and displays those superpixels on the image.
 //
 // Preconditions:
@@ -58,32 +89,15 @@ int main(int argc, char* argv[])
 	Ptr<SuperpixelSLIC> slic = createSuperpixelSLIC(cielab_image, SLIC, avg_superpixel_size, smoothness);
 	slic->iterate(1);
 	slic->enforceLabelConnectivity(min_superpixel_size_percent);
+
+	show_superpixels(slic, input_image, window_name);
+	
 	// 50.0 good for aguilles_rogues, 32.0 good for cosmo
 	slic->duperizeWithAverage(30.0);
 	// const int num_buckets[] = {16, 16, 16};
 	// slic->duperizeWithHistogram(num_buckets, 2.0f);
 
-	// Gets overlay image of superpixels
-	Mat superpixels;
-	slic->getLabelContourMask(superpixels);
-
-	// Creates the output image of superpixels
-	Mat output(input_image);
-	// Set each pixel in output to white if it's a superpixel border
-	for (int row = 0; row < output.rows; row += 1)
-	for (int col = 0; col < output.cols; col += 1)
-	{
-		if (superpixels.at<uchar>(row, col) != 0)
-		{
-			output.at<Vec3b>(row, col)[0] = superpixels.at<uchar>(row, col);
-			output.at<Vec3b>(row, col)[1] = superpixels.at<uchar>(row, col);
-			output.at<Vec3b>(row, col)[2] = superpixels.at<uchar>(row, col);
-		}
-	}
-
-	// Displays output to a window
-	imshow(window_name, output);
-	waitKey(0);
+	Mat output = show_superpixels(slic, input_image, window_name);
 	
 	// Write output to an image file
 	imwrite("output.png", output);
